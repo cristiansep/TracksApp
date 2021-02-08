df42333b5186df614822d4a5bead2901eb415fa3
cristiansep <cristiansepulvedamendez@gmail.com>
2021-02-08 13:14:06 -0300

    AuthContext added

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://b4bcd1a74e73.ngrok.io'
+})
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,16 +1,48 @@
 import createDataContext from "./createDataContext";
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
 
     switch (action.type) {
+        case 'add_error': 
+            return {...state, errorMessage: action.payload};
         default:
             return state;
     }
 };
 
 
+const signup = (dispatch) => {
+    return async({email, password}) => {
+
+        try {
+
+            const response = await trackerApi.post('/signup', {email, password});
+            console.log(response.data)
+            
+        } catch (error) {
+            dispatch({type: 'add_error', payload: 'Algo salio mal al tratar de registrarse'})
+        }
+    }
+};
+
+const signin = (dispatch) => {
+    return async ({email, password}) => {
+
+   
+
+    }
+};
+
+const signout = (dispatch) => {
+    return () => {
+
+    }
+}
+
+
 export const {Provider, Context} = createDataContext(
     authReducer,
-    {},
-    {isSignedIn: false}
+    {signin, signout, signup},
+    {isSignedIn: false, errorMessage: ''}
 );
